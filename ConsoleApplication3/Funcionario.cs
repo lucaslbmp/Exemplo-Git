cce32261855a8ba4d1a41766eae901af6e49588a
Lucas <lucasset10@msn.com>
2019-06-23 11:12:24 -0300

    Projeto iniciado

---
+++ ConsoleApplication3/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+        public double imposto;
+
+        public double SalarioLiquido()
+        {
+            return salario - imposto;
+        }
+
+        public double AumentarSalario(double porcentagem)
+        {
+            salario = (1 + porcentagem/100.0) * salario;
+            return salario;
+        }
+
+
+        public override string ToString()
+        {
+            return "Funcionario: " + nome + ", $" + SalarioLiquido();
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            Console.Write("Nome: ");
+            f.nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f.salario = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.imposto = double.Parse(Console.ReadLine());
+            Console.Write("\n"+f);
+            Console.Write("\nPercentual de aumento: ");
+            double porcent = double.Parse(Console.ReadLine());
+            f.AumentarSalario(porcent);
+            Console.Write("\n"+f);
+            Console.ReadLine();
+        }
+    }
+}
